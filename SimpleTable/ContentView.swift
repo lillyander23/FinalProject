235268bc2823b9635e6facdc36334977524390c8
lillyander23 <lillyanderson676@gmail.com>
2022-12-02 09:41:35 -0600

    Done

--- SimpleTable/ContentView.swift
+++ SimpleTable/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     let resturantsNames: [String] = ["Travail Kitchen and Amusements","Hai Hai","Nixta Tortilleria & Mexican takeout","Sammy’s Avenue Eatery","Kramarczuk Sausage Company","Union Hmong Kitchen","Soul Bowl","Spoon and Stable","Billy Sushi ビリー寿司","Restaurant Alma","Owamni by The Sioux Chef","Zen Box Izakaya","Manny's Steakhouse","Keefer Court","Pimento Jamaican Kitchen & Rum Bar","Lu's Sandwiches","Quang Restaurant","Sooki & Mimi","Manny's Tortas","Taqueria y Birrieria Las Cuatro Milpas"]
-    let images = ["Travail", "HaiHai","Nixta","Sammys","kramarczuk","hmongKitchen","soulbowl","spoonstable","billysushi","RestaurantAlma","Owamni","ZenBox","Mannys","keefer","Pimento","lus","Quang","sooki","Mannys","Taqueria"]
+    let images: [String] = ["Travail", "HaiHai","Nixta","Sammys","kramarczuk","hmongkitchen","soulbowl","spoonstable","billysushi","RestaurantAlma","Owamni","ZenBox","Mannys","keefer","Pimento","lus","Quang","sooki","Mannys","Taqueria"]
     var body: some View {
         VStack{
             Text ("Minneapolis Resturants")
@@ -20,13 +20,13 @@ struct ContentView: View {
         List {
             ForEach(0...resturantsNames.count - 1, id: \.self) { index in
                 HStack{
-                    Image("resturant")
+                    Image("\(images[index])")
                         .resizable()
                         .frame(width: 50, height: 50)
                         .cornerRadius(4)
+                    Text("\(resturantsNames[index])")
                 }
             }
-            .lifeStyle(.plain)
         }
     }
 }
