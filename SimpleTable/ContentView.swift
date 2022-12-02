1410c5e5a6bcf933233d6e239ad48eb7fba88c1e
lillyander23 <lillyanderson676@gmail.com>
2022-12-02 09:22:20 -0600

    taking a break

--- SimpleTable/ContentView.swift
+++ SimpleTable/ContentView.swift
@@ -8,17 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-        // Your variables here
-    
+    let resturantsNames: [String] = ["Travail Kitchen and Amusements","Hai Hai","Nixta Tortilleria & Mexican takeout","Sammy’s Avenue Eatery","Kramarczuk Sausage Company","Union Hmong Kitchen","Soul Bowl","Spoon and Stable","Billy Sushi ビリー寿司","Restaurant Alma","Owamni by The Sioux Chef","Zen Box Izakaya","Manny's Steakhouse","Keefer Court","Pimento Jamaican Kitchen & Rum Bar","Lu's Sandwiches","Quang Restaurant","Sooki & Mimi","Manny's Tortas","Taqueria y Birrieria Las Cuatro Milpas"]
+    let images = ["Travail", "HaiHai","Nixta","Sammys","kramarczuk","hmongKitchen","soulbowl","spoonstable","billysushi","RestaurantAlma","Owamni","ZenBox","Mannys","keefer","Pimento","lus","Quang","sooki","Mannys","Taqueria"]
     var body: some View {
-        Text("To Do.")
-        // Your code here
-        
+        VStack{
+            Text ("Minneapolis Resturants")
+                .font(.title3)
+                .fontWeight(.heavy)
+                .padding()
+        }
+        List {
+            ForEach(0...resturantsNames.count - 1, id: \.self) { index in
+                HStack{
+                    Image("resturant")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .cornerRadius(4)
+                }
+            }
+            .lifeStyle(.plain)
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
