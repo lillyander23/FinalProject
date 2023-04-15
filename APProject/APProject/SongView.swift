52bcf4f2dbf6aec2594f03e60d11bdd4873b1c69
lillyander23 <lillyanderson676@gmail.com>
2023-04-15 10:10:29 -0500

    working on error

--- APProject/APProject/SongView.swift
+++ APProject/APProject/SongView.swift
@@ -11,23 +11,24 @@ import SwiftUI
 
 struct SongView: View {
     
-    @State var songs = SongStored.songs()
-  //  songs = song. when hit the button to filter 
+@State var songs = SongStored.songs()
+@State var isFavorites: Bool = false
+    //  songs = song. when hit the button to filter
     var body: some View {
         
         NavigationView{
             
-                VStack{
-                    List(0..<songs.count, id: \.self) { index in
+            VStack{
+                List(0..<songs.count, id: \.self) { index in
+                    
+                    ZStack{
+                        Rectangle()
+                            .frame(width: .infinity, height: .infinity)
+                            .ignoresSafeArea(.all)
+                        Color("AccentColor2")
+                        //
                         
-                        ZStack{
-                            Rectangle()
-                                .frame(width: .infinity, height: .infinity)
-                                .ignoresSafeArea(.all)
-                            Color("AccentColor2")
-//
-                                
- 
+                        
                         VStack{
                             
                             Spacer()
@@ -49,45 +50,47 @@ struct SongView: View {
                                 }
                             )
                             HStack {
-                                Text("Release Date: \(songDetails[2])")
+                                Text("Release Date: \(songs[index].releaseDate)")
                                     .font(.subheadline)
                                     .foregroundColor(Color("AccentColor"))
                                 
                             }
                             HStack{
-                                NavigationLink(destination: DetailView(name: songDetails[1], artist: songDetails[0], imageURL: songDetails[3])
-                                ){
-                                    VStack {
-                                        Text("Want too hear a song clip?")
-                                            .font(.subheadline)
+                                NavigationLink(destination: DetailView(name: songs[index].songName, artist: songs[index].releaseDate, imageURL: songs[index].artworkUrl100)){
+                                   
+                                    {
+                                        VStack {
+                                            Text("Want too hear a song clip?")
+                                                .font(.subheadline)
+                                                .foregroundColor(Color("AccentColor"))
+                                                .frame(maxWidth: 450, maxHeight: .infinity, alignment: .leading)
+                                            Text("Click on the music note!")
+                                                .font(.subheadline)
+                                                .foregroundColor(Color("AccentColor"))
+                                                .frame(maxWidth: 450, maxHeight: .infinity, alignment: .leading)
+                                        }
+                                        Image(systemName: "music.note")
+                                            .padding()
                                             .foregroundColor(Color("AccentColor"))
-                                            .frame(maxWidth: 450, maxHeight: .infinity, alignment: .leading)
-                                        Text("Click on the music note!")
-                                            .font(.subheadline)
-                                            .foregroundColor(Color("AccentColor"))
-                                            .frame(maxWidth: 450, maxHeight: .infinity, alignment: .leading)
                                     }
-                                    Image(systemName: "music.note")
-                                        .padding()
-                                        .foregroundColor(Color("AccentColor"))
                                 }
                             }
                         }
+                        
                     }
                     
                 }
-                   
-            }
                 .toolbar{
                     Button(action: {isFavorites.toggle()}){
                         Text ("Filtered View")
-                            
+                        
                     }
                 }
-        }
-        
+            }
+            
         }
     }
+}
 
         struct SongView_Previews: PreviewProvider {
             static var previews: some View {
